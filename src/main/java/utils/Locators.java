a8efec058e10b81cfb3b0a7d5cc109ef41d93321
dench <sukhov.den@mail.ru>
2024-03-22 23:04:36 +0300

    1

---
+++ src/main/java/utils/Locators.java
@@ -0,0 +1,15 @@
+package utils;
+
+import org.openqa.selenium.By;
+
+public interface Locators {
+
+    public interface MainPage {
+        By BUTTON = By.xpath("(//div[contains(text(),'Почта')])[1]");
+    }
+
+    public interface SearchResultPage {
+        By HEADER_TEXT = By.xpath("(//div[@itemprop='name'])[1]");
+    }
+
+}
